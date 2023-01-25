3fac10f9dd895970005d5ca207c96e683b56b282
heznad <heznad@yandex.ru>
2023-01-26 03:12:38 +0700

    ДЗ 26.01

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,16 +1,22 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <set>
 
 using namespace std;
 
 int main() {
-    vector<int> elem = { -2,5,0,3,5,-2 };
-    set<int> elements(elem.begin(), elem.end());
-    for (int value : elements) {
-        cout << value << ' ';
-        }
-
-    return 0;
+vector<int> elements = { 1,6,828,-48,99,135,-7 };
+for (int i = 0; i < elements.size(); i++) {
+	for (int g = i+1; g < elements.size(); g++) {
+		if (elements[g] < elements[i]) {
+			int n = elements[i];
+			elements[i] = elements[g];
+			elements[g] = n;
+		}
+	}
+}
+for (int value : elements) {
+	cout << value << ' ';
+}
+return 0;
 }
