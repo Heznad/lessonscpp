1dcfdc8373cdb54d72c763b32f50fcc9f137cae8
heznad <heznad@yandex.ru>
2023-01-27 00:53:06 +0700

    добавил комментарии к map

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -17,9 +17,9 @@ int main() {
         {"червяк"s, 0}
     };
    /* animals.erase("собака"); */
-    animals["собака"] = 6;
-    animals.insert({ "кот", 4 });
-    animals.insert(make_pair("лошадь", 4));
+    animals["собака"] = 6; //поменять значение
+    animals.insert({ "кот", 4 }); //добавить пару
+    animals.insert(make_pair("лошадь", 4)); //добавить пару
     for (const auto& [key, value]: animals) {
         if (value == 4) {
             cout << key << ' ' << value << endl;
