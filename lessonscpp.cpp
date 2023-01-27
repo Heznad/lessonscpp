112d5fcc0341568c30e4de745d3c64c062ea3c47
heznad <heznad@yandex.ru>
2023-01-28 03:11:10 +0700

    ДЗ 28.01
    #1 Написать функцию, вычисляющую среднее арифметическое элементов вектора. Элементы вектора - вещественные числа.
    
    #2 Оформить в виде функции решенную задачу о замене ключей и значений map.
    
    #3 Написать функцию, возвращающую все положительные элементы вектора без повторов

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,39 +1,73 @@
 ﻿#include <iostream>
 #include <vector>
+#include <map>
+#include <set>
 
 using namespace std;
 
-// int - тип возвращаемого значения (то, что поле return)
-// Sum - название Функции
-// int a, int b - принимаемые функцией переменные
-int Sum(int first, int second) {
-    int result = first + second;
-    return result;
+double SredArifm(vector<double> elements) {
+	if (elements.empty()) {
+		cout << "Контейнер пуст." << endl;
+		return 0;
+	}
+	int i = 0;
+	double sum = 0;
+	for (double value : elements) {
+		i++;
+		sum = sum+value;
+	}
+	double sredarifm = sum / i;
+	return sredarifm;
 }
 
-int MinElement(vector<int> elements) {
-    if (elements.empty()) {
-        return 0;
-    }
-    int min = elements[0];
-    for (int value : elements) {
-        if (min > value) {
-            min = value;
-        }
-    }
-    return min;
+void Swap(map<string, string > changepos) {
+	map<string, string> noise;
+	for (const auto& [key, value] : changepos) {
+		noise.insert({ value,key });
+	};
+	for (const auto& [key, value] : noise) {
+		cout << key << ' ' << value << endl;
+	}
 }
 
-void Print(int printed) {
-    cout << printed << endl;
+void PologElementi(vector<int>elements) {
+	set<int> elem(elements.begin(), elements.end());
+	for (int value : elem) {
+		if (value > 0) {
+			cout << value << ' '<<endl;
+		}
+	}
 }
 
 int main() {
-    vector<int> elements{25, 72, -1, 100};
-    cout << MinElement(elements)<< endl;
-    //int a = 2;
-    //int b = 3;
-    //int c = Sum(a, b);
-    //Print(c);
-    return 0;
+
+	setlocale(LC_ALL, "ru");
+	
+	/* #1 Написать функцию, вычисляющую среднее арифметическое элементов вектора.
+Элементы вектора - вещественные числа.*/
+
+	vector<double> elements{ 85.6, 32.2, 97.001, 437, 105, 27 };
+	cout<< SredArifm(elements)<<endl;
+
+	cout << "=============================================" << endl;
+
+	/* #2 Оформить в виде функции решенную задачу о замене ключей и значений map. */
+
+	map<string, string > sounds = { {"cat"s, "meow"s},
+										{"dog"s, "bark"s},
+										{"cow"s, "moo"s},
+										{"owl"s, "hoot"s},
+										{"tiger"s, "roar"s} };
+	
+	Swap(sounds);
+	
+	cout << "=============================================" << endl;
+
+	/* #3 Написать функцию, возвращающую все положительные элементы вектора без повторов*/
+
+	vector<int> elem = { 5,90,-232,54,23,5,23,1,90,-65 };
+
+	PologElementi(elem);
+
+	return 0;
 }
