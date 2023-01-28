c17b11388c76d2192e08c74948b02abbddddc956
heznad <heznad@yandex.ru>
2023-01-29 02:38:22 +0700

    ДЗ 29.01 Создать переменную, хранящую вещественное число. 1) Создать указатель на эту переменную и прибавить к ней 5 через указатель, не обращаясь к её имени напрямую. Т.е. нельзя написать value += 5. Нужно взаимодействие с указателем. 2) Создать ссылку на переменную и через ссылку умножить переменную на -1

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,78 +1,29 @@
 ﻿#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
+
+
+/*Задание
+Создать переменную, хранящую вещественное число.*/
+/*1) Создать указатель на эту переменную и прибавить к ней 5 через указатель,
+не обращаясь к её имени напрямую.Т.е.нельзя написать value += 5. Нужно взаимодействие с указателем.*/
+
 
 using namespace std;
 
-double SredArifm(vector<double> elements) {
-	if (elements.empty()) {
-		cout << "Контейнер пуст." << endl;
-		return 0;
-	}
-	int i = 0;
-	double sum = 0;
-	for (double value : elements) {
-		i++;
-		sum = sum+value;
-	}
-	double sredarifm = sum / i;
-	return sredarifm;
-}
-
-map<string, string> Swap(map<string, string> changepos) {
-	map<string, string> noise;
-	for (const auto& [key, value] : changepos) {
-		noise.insert({ value,key });
-	}
-	return noise;
-}
-
-set<int> PologElementi(vector<int>elements) {
-	set<int> elem;
-	for (int value : elements) {
-		if (value > 0) {
-			elem.insert(value);
-		}
-	}
-	return elem;
-}
-
 int main() {
+	/*1) Создать указатель на эту переменную и прибавить к ней 5 через указатель,
+не обращаясь к её имени напрямую.Т.е.нельзя написать value += 5. Нужно взаимодействие с указателем.*/
+	double a = 9.58;
+	double* ukaz_a = &a;
+	*ukaz_a += 5;
+	cout << " a = " << *ukaz_a << endl;
 
-	setlocale(LC_ALL, "ru");
-	
-	/* #1 Написать функцию, вычисляющую среднее арифметическое элементов вектора.
-Элементы вектора - вещественные числа.*/
+	cout << "=========================================" << endl;
 
-	vector<double> elements{ 85.6, 32.2, 97.001, 437, 105, 27 };
-	cout<< SredArifm(elements)<<endl;
+	/*2) Создать ссылку на переменную и через ссылку умножить переменную на - 1*/
 
-	cout << "=============================================" << endl;
+	double& ssilka_a = a;
+	ssilka_a = ssilka_a * (-1);
+	cout << " a = " << ssilka_a << endl;
 
-	/* #2 Оформить в виде функции решенную задачу о замене ключей и значений map. */
-
-	map<string, string > sounds = { {"cat"s, "meow"s},
-										{"dog"s, "bark"s},
-										{"cow"s, "moo"s},
-										{"owl"s, "hoot"s},
-										{"tiger"s, "roar"s} };
-	
-	//map<string,string>noise = Swap(sounds);
-	for (const auto&[key,value] : Swap(sounds)) {
-		cout << key << ' ' << value << endl;
-	}
-	
-	cout << "=============================================" << endl;
-
-	/* #3 Написать функцию, возвращающую все положительные элементы вектора без повторов*/
-
-	vector<int> elem = { 5,90,-232,54,23,5,23,1,90,-65 };
-
-	set<int>ele = PologElementi(elem);
-	for (int value : ele ) {
-		cout << value << endl;
-	}
-
-	return 0;
+	return 0; 
 }
