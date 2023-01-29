edec8125909a13bb372bc9c7088314d321774b6d
heznad <heznad@yandex.ru>
2023-01-30 05:43:16 +0700

    ДЗ 30.01 Задание #1 Написать функцию вычисления суммы всех положительных элементов вектора. Результат дописать в конец вектора. В main вывести последний элемент вектора (результат суммы). Написать тесты для этой функции
    #2 Внутри функции посчитать квадрат суммы двух чисел. Функция ничего не должна возвращать. Результат вычисления записать во вторую переменную. Вывести результат в main. Тесты к этой функции не обязательны
    
    #3 Написать функцию удаления из строки пробелов. Функция должна возвращать строку без пробелов. Написать тесты.
    Пример: "Hello world" => "Helloworld"

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,61 +1,92 @@
-﻿#include <chrono>
-#include <iostream>
-#include <cassert>
+﻿#include <iostream>
 #include <vector>
-#include <numeric>
+#include <cassert>
 
 using namespace std;
 
-class LogDuration {
-public:
-    using Clock = std::chrono::steady_clock;
-
-    LogDuration(const std::string name_block, std::ostream& output = std::cerr)
-        : name_block_(name_block) {
-    }
-
-    ~LogDuration() {
-        using namespace std::chrono;
-        using namespace std::literals;
-
-        const auto end_time = Clock::now();
-        const auto dur = end_time - start_time_;
-        std::cerr << name_block_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
-    }
-
-private:
-    const Clock::time_point start_time_ = Clock::now();
-    const std::string name_block_;
-};
-
-double AverageCopy(vector<double> elements) {
-    double result = 0.0;
-    for (const double& value : elements) {
-        result += value;
-    }
-    return result /= elements.size();
+void TestVector(vector<int> elements) {
+	vector<int> etalon{ 1,-2,-3,4,5,-6,7,-8,9,10,36 };
+	assert(elements == etalon);
 }
 
-double AverageRef(const vector<double>& elements) {
-    double result = 0.0;
-    for (const double& value : elements) {
-        result += value;
-    }
-    return result /= elements.size();
+int Sum(vector<int>& elements) {
+	if (elements.empty()) {
+		cout << "Котейнер пуст" << endl;
+		return 0;
+	}
+
+	int sum = 0;
+	for (int& value : elements) {
+		if (value > 0) {
+			sum = sum + value;
+		}
+	}
+	elements.insert(elements.end(), sum);
+	TestVector(elements);
+	return sum;
+}
+
+
+
+void Square(int& a, int& b) {
+	b = (b + a) * (b + a);
+}
+ 
+void TestStroki(string text) {
+	string etalon = "MyNameVitalya";
+	assert(text == etalon);
+}
+
+string BezProbelov(string text) {
+
+	text.erase(remove(text.begin(), text.end(), ' '), text.end());
+	TestStroki(text);
+	return text;
 }
 
 int main() {
-    const size_t size = 1000000000;
-    vector<double> test(size);
-    iota(test.begin(), test.end(), 1);
-    {
-        LogDuration timer("Copy");
-        cout << AverageCopy(test) << endl;
-    }
-    {
-        LogDuration timer("NO copy");
-        cout << AverageRef(test) << endl;
-    }
 
-    return 0;
+	setlocale(LC_ALL, "ru");
+	cout << "===========================================" << endl;
+
+	/* #1 Написать функцию вычисления суммы всех положительных элементов вектора.
+Результат дописать в конец вектора.В main вывести последний элемент вектора(результат суммы).
+Написать тесты для этой функции */
+
+	vector<int> elements{ 1,-2,-3,4,5,-6,7,-8,9,10 };
+	cout << "Контейнер без изменений = ";
+	for (const int& value : elements) {
+		cout << value << ' ';
+	}
+	cout << endl;
+	cout << "Сумма положительныз чисел = " << Sum(elements) << endl;
+	cout << "Добавлен элемент суммы всех положительных чисел = ";
+	for (const int value : elements) {
+		cout << value << ' ';
+	}
+	cout << endl;
+
+	cout << "===========================================" << endl;
+	
+	/* #2 Внутри функции посчитать квадрат суммы двух чисел.
+Функция ничего не должна возвращать.
+Результат вычисления записать во вторую переменную.
+Вывести результат в main.Тесты к этой функции не обязательны */
+
+	int a = 5;
+	int b = 10;
+	Square(a, b);
+	cout << "Квадрат суммы a и b = " << b << endl;
+
+	cout << "===========================================" << endl;
+
+	/* #3 Написать функцию удаления из строки пробелов.
+Функция должна возвращать строку без пробелов.Написать тесты.
+Пример: "Hello world" = > "Helloworld" */
+
+	string text = "My Name Vitalya";
+	cout << "С пробелами = " << text << endl;
+	cout << "Без пробелов = " << BezProbelov(text) << endl;
+
+	return 0;
 }
