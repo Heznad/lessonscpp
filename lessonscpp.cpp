1b098d35d1314a8f00dd3b652721335ab0c83203
heznad <heznad@yandex.ru>
2023-01-29 16:18:54 +0700

    Показательный пример сколько программа затратитть времени на копирование и на ссылку при внесениии вектора в функции.

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,31 +1,61 @@
-﻿#include <iostream>
+﻿#include <chrono>
+#include <iostream>
+#include <cassert>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
-//Задание. Четные позиции вектора занулить
+class LogDuration {
+public:
+    using Clock = std::chrono::steady_clock;
 
-void ObnulChet(vector<int>& vector) {
-	for (int i = 0; i < vector.size();i++) {
-		if (i % 2 == 0) {
-			vector[i] = 0;
-		}
-	}
+    LogDuration(const std::string name_block, std::ostream& output = std::cerr)
+        : name_block_(name_block) {
+    }
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        std::cerr << name_block_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+    }
+
+private:
+    const Clock::time_point start_time_ = Clock::now();
+    const std::string name_block_;
+};
+
+double AverageCopy(vector<double> elements) {
+    double result = 0.0;
+    for (const double& value : elements) {
+        result += value;
+    }
+    return result /= elements.size();
 }
 
-void Print(const vector<int>& elements) {
-	for (const int& value : elements) {
-		cout << value << ' ';
-	}
-	cout << endl;
+double AverageRef(const vector<double>& elements) {
+    double result = 0.0;
+    for (const double& value : elements) {
+        result += value;
+    }
+    return result /= elements.size();
 }
 
 int main() {
-	vector<int> vector{ 7,4,19,32,0,47,87,0,-12,523,-61,20 };
-	cout << "before" << endl;
-	Print(vector);
-	cout << "after" << endl;
-	ObnulChet(vector);
-	Print(vector);
-	return 0;
+    const size_t size = 1000000000;
+    vector<double> test(size);
+    iota(test.begin(), test.end(), 1);
+    {
+        LogDuration timer("Copy");
+        cout << AverageCopy(test) << endl;
+    }
+    {
+        LogDuration timer("NO copy");
+        cout << AverageRef(test) << endl;
+    }
+
+    return 0;
 }
