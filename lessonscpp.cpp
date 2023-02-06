63fc8626c47d4276fe0b8bac1c12aa23dafe9bb9
heznad <heznad@yandex.ru>
2023-02-07 00:15:48 +0700

    Динамический массив

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,42 +1,15 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <cassert>
-#include <string>
-#include <vector>
 
 using namespace std;
 
-void WriteToFile(ostream& out, const string& text) {
-    out << text;
-}
-
-vector<string> ReadFromFile(istream& out) {
-    vector<string> result;
-    string str;
-    while (!out.eof()) // пока файл не пуст, считываем из файла по одному символу и выводим на экран
-    {
-       /* getline(out, str);
-        result.emplace_back(str);*/
-
-        result.emplace_back();
-        getline(out, result.back());
-        
-    }
-    return result;
-}
-
-
 int main() {
-   /* ofstream fout("test.txt"s);
-    string str = "sdhfjhskd";
-    WriteToFile(fout, str);
-    fout.close();*/
-
-    ifstream iout("test.txt"s);
-
-    for (string value : ReadFromFile(iout) ) {
-        cout << value << '\n';
+    int size = 0;
+    cout << "Enter size array: ";
+    cin >> size;
+    int* arr = new int[size];
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << ' ';
     }
+
     return 0;
 }
