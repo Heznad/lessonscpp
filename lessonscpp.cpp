072379ba2ee9bc7817bc642937de5d452c7744e1
heznad <heznad@yandex.ru>
2023-02-10 03:20:52 +0700

    Парсинг слов в вектор со строки

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,15 +1,44 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main() {
-    int size = 0;
-    cout << "Enter size array: ";
-    cin >> size;
-    int* arr = new int[size];
-    for (int i = 0; i < size; ++i) {
-        cout << arr[i] << ' ';
-    }
+vector<string> Parcing(string& str) {
+	vector<string> str1;
+	string b;
+	str += " ";
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] != ' ') {
+			b += str[i];
+		}
+		else{
+			str1.push_back(b);
+			b.clear();
+		}
+	}
+	return str1;
+}
 
-    return 0;
+int main() {
+	string str = "vd0 ,s.l 8r5f isvbd privet  poka";
+	for (string value : Parcing(str)) {
+		cout << value << '\n';
+	}
+
+	cout << "======================" << endl;
+
+	string str1 = " ";
+	for (string value : Parcing(str1)) {
+		cout << value << '\n';
+	}
+
+	cout << "======================" << endl;
+
+	string str2 = "b";
+	for (string value : Parcing(str2)) {
+		cout << value << '\n';
+	}
+
+	return 0;
 }
