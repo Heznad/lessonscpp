1bba42c7b38649c975a8ee29650a0d194be64968
heznad <heznad@yandex.ru>
2023-02-10 18:27:57 +0700

    Доделанный парсинг с тестами

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,13 +1,16 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
+#include <cassert>
 
 using namespace std;
 
-vector<string> Parcing(string& str) {
+vector<string> Parcing(const string& str) {
 	vector<string> str1;
+	if (str.empty()) {
+		return str1;
+	}
 	string b;
-	str += " ";
 	for (int i = 0; i < str.size(); i++) {
 		if (str[i] != ' ') {
 			b += str[i];
@@ -17,10 +20,32 @@ vector<string> Parcing(string& str) {
 			b.clear();
 		}
 	}
+	if (!b.empty()) {
+		str1.push_back(b);
+	}
 	return str1;
 }
 
+void Test() {
+	{
+		string text = "First"s;
+		vector<string> result = { "First"s };
+		assert(Parcing(text) == result);
+	}
+	{
+		string text = "Hello Google my name is Boba I am coder c++"s;
+		vector<string> result = { "Hello"s, "Google"s, "my"s, "name"s, "is"s, "Boba"s, "I"s, "am"s, "coder"s, "c++"s };
+		assert(Parcing(text) == result);
+	}
+	{
+		string text;
+		vector<string> result = Parcing(text);
+		assert(result.empty());
+	}
+}
+
 int main() {
+	Test();
 	string str = "vd0 ,s.l 8r5f isvbd privet  poka";
 	for (string value : Parcing(str)) {
 		cout << value << '\n';
