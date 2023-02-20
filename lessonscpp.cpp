d0109c48d83b92d1da98f0712f88e7d648aa8bff
Heznad <heznad@yandex.ru>
2023-02-20 16:22:19 +0700

    Работа с итераторами Вывести значения до отрицательного числа, и после отрицательного

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,69 +1,34 @@
 ﻿#include <iostream>
 #include <vector>
-#include <string>
-#include <cassert>
 
 using namespace std;
 
-vector<string> Parcing(const string& str) {
-	vector<string> str1;
-	if (str.empty()) {
-		return str1;
-	}
-	string b;
-	for (int i = 0; i < str.size(); i++) {
-		if (str[i] != ' ') {
-			b += str[i];
-		}
-		else{
-			str1.push_back(b);
-			b.clear();
-		}
-	}
-	if (!b.empty()) {
-		str1.push_back(b);
-	}
-	return str1;
-}
-
-void Test() {
-	{
-		string text = "First"s;
-		vector<string> result = { "First"s };
-		assert(Parcing(text) == result);
-	}
-	{
-		string text = "Hello Google my name is Boba I am coder c++"s;
-		vector<string> result = { "Hello"s, "Google"s, "my"s, "name"s, "is"s, "Boba"s, "I"s, "am"s, "coder"s, "c++"s };
-		assert(Parcing(text) == result);
-	}
-	{
-		string text;
-		vector<string> result = Parcing(text);
-		assert(result.empty());
-	}
-}
-
 int main() {
-	Test();
-	string str = "vd0 ,s.l 8r5f isvbd privet  poka";
-	for (string value : Parcing(str)) {
-		cout << value << '\n';
+	setlocale(LC_ALL, "ru");
+	vector<int> elements = { 2, 4, 9, -5, 10, 6, 4 };
+	vector<int> before;
+	
+	vector<int>::const_iterator it = elements.begin();
+	vector<int>::const_iterator it_end = elements.end();
+
+
+	while (*it >= 0) {
+		before.push_back(*++it);
+	}
+	vector<int> after(it+1, it_end);
+
+	cout << "до" << endl;
+
+	for (int const value : before) {
+		cout << value << "\t";
 	}
 
-	cout << "======================" << endl;
+	cout << endl;
 
-	string str1 = " ";
-	for (string value : Parcing(str1)) {
-		cout << value << '\n';
+	cout << "после" << endl;
+
+	for (int const value : after) {
+			cout << value << "\t";
 	}
-
-	cout << "======================" << endl;
-
-	string str2 = "b";
-	for (string value : Parcing(str2)) {
-		cout << value << '\n';
-	}
-
-	return 0;
+	
 }
