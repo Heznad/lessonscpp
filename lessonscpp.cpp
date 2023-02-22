21d928f28bc73416ba0e719182f64d6c38ad019c
Heznad <heznad@yandex.ru>
2023-02-22 19:21:03 +0700

    ДЗ 19.02 Двумерный массив 1. Сумма элементов побочной диагонали 2. Сумма элементов над побочной диагональю 3. Сумма под побочной диагональю

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -3,30 +3,35 @@
 using namespace std;
 
 int main() {
-    int size;
-    cin >> size;
-    int** arr = new int* [size];
-    for (int i = 0; i < size; ++i) {
-        arr[i] = new int[size];
-    }
-
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            cin >> arr[i][j];
-        }
-        cout << endl;
-    }
-
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            cout << arr[i][j] << ' ';
-        }
-        cout << endl;
-    }
-
-    for (int i = 0; i < size; ++i) {
-        delete[] arr[i];
-    }
-    delete[] arr;
-    return 0;
+	setlocale(LC_ALL, "ru");
+	int const size = 3;
+	int arr[size][size] = {
+		{2,5,0},
+		{7,-2,10},
+		{5,6,20}
+	};
+	cout << "Матрица:\n";
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	int sump = 0;
+	int sumpu = 0;
+	int sumpd = 0;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (i + j == size - 1) {
+				sump += arr[i][j];
+			}
+			if (i < size - 1 - j) {
+				sumpu += arr[i][j];
+			}
+			if (i > size - 1 - j) {
+				sumpd += arr[i][j];
+			}
+		}
+	}
+	cout << "Сумма элементов побочной диагонали:\t"<< sump << "\nСумма элементов над побочной диагональю:\t"<<sumpu<<"\nСумма под побочной диагональю:\t"<<sumpd<<endl;
 }
