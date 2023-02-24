592b5dfa889747ae25acff0404d1325a37872db0
Heznad <heznad@yandex.ru>
2023-02-24 15:12:12 +0700

    Динамический двумерный массив Функции выделения и освобождения памяти для двумерных массивов

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -2,36 +2,35 @@
 
 using namespace std;
 
+
+double** AllocationMemory(int str,int col)  {
+    double** arr = new double* [str];
+    for (int i = 0; i < str; ++i) {
+        arr[i] = new double[col];
+    }
+    return arr;
+}
+
+void FreeMemory(double** arr, int str) {
+    for (int i = 0; i < str; ++i) {
+        delete[] arr[i];
+    }
+}
+
 int main() {
-	setlocale(LC_ALL, "ru");
-	int const size = 3;
-	int arr[size][size] = {
-		{2,5,0},
-		{7,-2,10},
-		{5,6,20}
-	};
-	cout << "Матрица:\n";
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
-	}
-	int sump = 0;
-	int sumpu = 0;
-	int sumpd = 0;
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			if (i + j == size - 1) {
-				sump += arr[i][j];
-			}
-			if (i < size - 1 - j) {
-				sumpu += arr[i][j];
-			}
-			if (i > size - 1 - j) {
-				sumpd += arr[i][j];
-			}
-		}
-	}
-	cout << "Сумма элементов побочной диагонали:\t"<< sump << "\nСумма элементов над побочной диагональю:\t"<<sumpu<<"\nСумма под побочной диагональю:\t"<<sumpd<<endl;
+    int str, col;
+    cout << "Vvedite kolichestvo strochek: ";
+    cin >> str;
+    cout << "Vvedite kolichestvo colonok: ";
+    cin >> col;
+    double** arr = AllocationMemory(str, col);
+    for (int i = 0; i < str; ++i) {
+        for (int j = 0; j < col; ++j) {
+            arr[i][j] = 6.5;
+            cout << arr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    FreeMemory(arr, str);
+    return 0;
 }
