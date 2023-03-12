fa04b4a7490f0db26d75c8d5421110c0fad428ad
Heznad <heznad@yandex.ru>
2023-03-12 18:18:51 +0700

    Классы Конструктор и деструктор

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,106 +1,60 @@
 ﻿#include <iostream>
-#include <vector>
 #include <string>
-#include <cassert>
-
-
-/*Доработать парсинг строк. Если строка пустая, то бросать исключение и обрабатывать его
-Или строка состоит только из пробелов
-Дописать тест для проверки на выброс исключений
-Проверить шаблон выделения памяти двумерного массива*/
+#include <vector>
 
 using namespace std;
 
-void Empty(const string& str) {
-	if (str.empty()) {
-		throw logic_error("Sentence is empty! ");
-	}
-}
+class People {
+public:
 
-void OnlyProbels(const string& str) {
-	int pr = 0;
-	for (int i = 0; i < str.length(); i++) {
-		if (str[i] != ' ') {
-			pr += 1;
-		}
-	}
-	if (pr == 0) {
-		throw logic_error("Sentence includes only probels! ");
-	}
-}
+    People(const string& name) {
+        name_ = name;
+        cout << "Hello, my name is " << name_ << endl;
+    }
 
-vector<string> Parcing(const string& str) {
-	vector<string> str1;
-	Empty(str);
-	OnlyProbels(str);
-	if (str.empty()) {
-		return str1;
-	}
-	string b;
-	for (int i = 0; i < str.size(); i++) {
-		if (str[i] != ' ') {
-			b += str[i];
-		}
-		else {
-			str1.push_back(b);
-			b.clear();
-		}
-	}
-	if (!b.empty()) {
-		str1.push_back(b);
-	}
-	return str1;
-}
+    ~People() {
+        cout << name_ << " go away" << endl;
+    }
+
+    People(const People& another) {
+        age_ = another.age_;
+        name_ = another.name_ + "_clon";
+        cout << "Hello, my name is " << name_ << endl;
+    }
+
+    string GetName() const {
+        return name_;
+    }
+
+    void SetName(const string& name) {
+        name_ = name;
+    }
+
+    int GetAge() const {
+        return age_;
+    }
+
+    int GetID() const {
+        return id_pass_;
+    }
 
 
 
-void Test() {
-	{
-		string text = "First"s;
-		vector<string> result = { "First"s };
-		assert(Parcing(text) == result);
-	}
-	{
-		string text = "Hello Google my name is Boba I am coder c++"s;
-		vector<string> result = { "Hello"s, "Google"s, "my"s, "name"s, "is"s, "Boba"s, "I"s, "am"s, "coder"s, "c++"s };
-		assert(Parcing(text) == result);
-	}
-	{
-		try {
-			string text;
-			vector<string> result = Parcing(text);
-			assert(false);
-		}
-		catch (const exception& isk) {
-		}
-	}
-}
+private:
+    int age_ = 10;
+    string name_ = "no name";
+    int id_pass_ = 1111;
+};
 
 int main() {
-	try {
-		Test();
-		string str = "vd0 ,s.l 8r5f isvbd privet  poka";
-		for (string value : Parcing(str)) {
-			cout << value << '\n';
-		}
+     //People first("George"s);
+     //People second(first);
+     vector<People> group;
+     group.reserve(10);
+     group.emplace_back("Li"s);
+     group.emplace_back("Pasha"s);
+     group.emplace_back("Alixan"s);
+     
 
-		cout << "======================" << endl;
-
-		string str1 = " ";
-		for (string value : Parcing(str1)) {
-			cout << value << '\n';
-		}
-
-		cout << "======================" << endl;
-
-		string str2 = "b";
-		for (string value : Parcing(str2)) {
-			cout << value << '\n';
-		}
-	}
-	catch (const exception& isk) {
-		cout << isk.what() << endl;
-	}
-
-	return 0;
+    return 0;
 }
