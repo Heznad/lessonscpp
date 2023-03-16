fe1daca1da1b87d0c14edfda0a0a21e24a37c1dd
Heznad <heznad@yandex.ru>
2023-03-16 14:50:59 +0700

    Доработанный класс-обёртка для массива + добавлена перегрузка оператора вывода <<

--- lessonscpp.cpp
+++ lessonscpp.cpp
@@ -1,20 +1,21 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
-
 using namespace std;
 
 /*Задание
 Написать конструкторы:
-1) который будет инициализировать наш массив значениями по умолчанию 
+1) который будет инициализировать наш массив значениями по умолчанию
 2) который будет инициализировать наш массив заданным значением
 3) написать метод вывода значений в консоль
 vector<int> a(5)*/
 
+
+
 class Massive {
 public:
 
-    Massive(const int& volume) {
+    Massive(int volume) {
         size = volume;
         arr = new int[size];
         for (int i = 0; i < size; ++i) {
@@ -22,7 +23,7 @@ public:
         }
     }
 
-    Massive(const int& volume,const int& b) {
+    Massive(int volume, int b) {
         size = volume;
         arr = new int[size];
         for (int i = 0; i < size; ++i) {
@@ -44,12 +45,16 @@ public:
 
     }
 
-    void Vivod() {
-        cout << "Massive: ";
-        for (int i = 0; i < size; ++i) {
-            cout << arr[i] << ' ';
-        }
-        cout << endl;
+    int Size() const{
+        return size;
+    }
+
+    int& operator[](int i) {
+        return arr[i];
+    }
+
+    const int& operator[](int i) const {
+        return arr[i];
     }
 
 private:
@@ -57,14 +62,21 @@ private:
     int size;
 };
 
+ostream& operator<<(ostream& out, const Massive& mas) {
+    for (int i = 0; i < mas.Size(); ++i) {
+        out << mas[i] << ' ';
+    }
+    return out;
+}
+
 int main() {
     int size = 0;
     cout << "Enter size array: ";
     cin >> size;
     Massive mas(size);
     mas.EditElement();
-    mas.Vivod();
+    cout << mas << endl;
     Massive mas1(size, 10);
-    mas1.Vivod();
+    cout << mas1 << endl;
     return 0;
 }
